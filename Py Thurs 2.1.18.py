e9f3c0921f2f8b68b1934dd05165ee03eeb56550
Jen Chojna <jchojna@oshift.com>
2018-02-01 12:40:41 -0500

    2 Data Types 1.25.18 with push - Added casting examples

--- Py Thurs 2.1.18.py
+++ Py Thurs 2.1.18.py
@@ -1,10 +1,39 @@
 # Data types
+
+# strings
 _str = "test"
-_str2 = ""
-Test
-for multiple lines""
+
+#booleans
 _bool = True
 _bool_false = False
+
+#integer
 _int = 1
 _int_neg = -1
-_float =
+_float = 2.8
+
+# casting
+
+output = int('1') + 1
+output2 = '1' + str(1)
+# this will not work, '1' is a string and 1 is a number
+
+print(bool(0))
+print(bool(1))
+print(bool(.1))
+print(bool('false'))
+print(bool(None))
+print(bool([]))
+
+# list
+_list = ['Alex', 'Grande', 'Sarah', 'Jen', 'Laura', 'Justin', 'Danielle']
+
+_list2 = list()
+
+# for {variable_name} in <collection>:
+#   <action>
+# Used for lists (Ex: counts = [1,2,3,4,5]
+# for count in counts
+name = 'name'
+for character in name:
+    print(character)
